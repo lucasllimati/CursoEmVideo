440a68139f852ae3447f7b4d425f137fba45f2b0
lucasllimati <lucasllimati@gmail.com>
2020-08-28 13:21:23 -0300

    ecercicio

--- ex017.py
+++ ex017.py
@@ -1,2 +1,17 @@
-# EXERCÍCIO
-#
+# EXERCÍCIO 17
+# Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
+
+from math import sqrt, pow, hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+
+#Forma 1
+hi = hypot(co, ca)
+
+#Forma 2
+#hi = sqrt(pow(cao, 2) + pow(ca, 2))
+
+#Forma 3
+#hi = (co ** 2 + ca ** 2) ** (1/2)
+
+print('A hipotenusa vai medir {:.2f}.'.format(hi))
--- ex018.py
+++ ex018.py
@@ -1,2 +1,14 @@
-# EXERCÍCIO
-#
+# EXERCÍCIO 18
+# Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+import math
+
+ang = int(input('Digite o ângulo que deseja: '))
+
+sen = math.sin(math.radians(ang))
+cos = math.cos(math.radians(ang))
+tg = math.tan(math.radians(ang))
+
+print('O ângulo de {} tem o SENO de {:.2f}'.format(ang, sen))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(ang, cos))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(ang, tg))
