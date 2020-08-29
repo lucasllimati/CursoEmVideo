78b4c2a734b2e5e806462cf50284faa66074fb7c
lucasllimati <lucasllimati@gmail.com>
2020-08-29 16:14:16 -0300

    exercicio

--- ex024.py
+++ ex024.py
@@ -1,2 +1,5 @@
-# EXERCÍCIO 2
-#
+# EXERCÍCIO 24
+# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”
+
+nome = str(input('Digite o nome da cidade: ')).upper().strip()
+print('SANTO' in nome)
