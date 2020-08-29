73edf1b94647ceb7522ea49e27d5910ea0d641c8
lucasllimati <lucasllimati@gmail.com>
2020-08-29 13:30:58 -0300

    exercicios

--- ex022.py
+++ ex022.py
@@ -1,2 +1,16 @@
 # EXERCÍCIO 22
-# Analisador de textos
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# O nome com todas as letras maiúsculas e minúsculas.
+# Quantas letras ao todo (sem considerar espaços).
+# Quantas letras tem o primeiro nome.
+
+#strip - remove espaços desnecessários
+nome = str(input('Digite o seu nome completo: ')).strip()
+pnome = nome.split()
+
+print('Nome completo: {}'.format(nome))
+print('Nome nome em maiúsculo é: {}'.format(nome.upper()))
+print('Nome nome em minúsculo é: {}'.format(nome.lower()))
+print('Seu nome completo possui {} letras'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome é {} e possui {} letras'.format(pnome[0].upper(), len(pnome[0])))
+
