6e9c9d811f8b6a79691c62969fe8ac7f6b307e8e
lucasllimati <lucasllimati@gmail.com>
2020-08-29 16:30:53 -0300

    exercicios

--- ex024.py
+++ ex024.py
@@ -2,4 +2,7 @@
 # Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”
 
 nome = str(input('Digite o nome da cidade: ')).upper().strip()
-print('SANTO' in nome)
+print('Começa com a palavra SANTO: {}'.format(nome[:5] == 'SANTO'))
+
+#MELHORIA - VERIFICA SE POSSUI SANTO NA FRASE INTEIRA
+print('Possui SANTO no nome: {}'.format('SANTO' in nome))
