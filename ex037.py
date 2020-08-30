2b09c747789cd5158bf605974e82665f9abe45f7
lucasllimati <lucasllimati@gmail.com>
2020-08-30 19:41:00 -0300

    exercicios

--- ex037.py
+++ ex037.py
@@ -8,10 +8,10 @@ print('\t[ 1 ] - Binário\n\t[ 2 ] - Octal\n\t[ 3 ] - Hexadecimal')
 opc = int(input('Sua opção: '))
 
 if opc == 1:
-    print('\33[34m{}\33[m convertido para \33[34mBINÁRIO\33[m é igual a \33[34m{}\33[m.'.format(num, bin(num)[2:]))
+    print('\33[36m{}\33[m convertido para \33[36mBINÁRIO\33[m é igual a \33[36m{}\33[m.'.format(num, bin(num)[2:]))
 elif opc == 2:
-    print('\33[34m{}\33[m convertido para \33[34mOCTAL\33[m é igual a \33[34m{}\33[m.'.format(num, oct(num)[2:]))
+    print('\33[36m{}\33[m convertido para \33[36mOCTAL\33[m é igual a \33[36m{}\33[m.'.format(num, oct(num)[2:]))
 elif opc == 3:
-    print('\33[34m{}\33[m convertido para \33[34mHEXADECIMAL\33[m é igual a \33[34m{}\33[m.'.format(num, hex(num)[2:]))
+    print('\33[36m{}\33[m convertido para \33[36mHEXADECIMAL\33[m é igual a \33[36m{}\33[m.'.format(num, hex(num)[2:]))
 else:
     print('\33[31m Opção inválida!Tente novamente.\33[m')
--- ex038.py
+++ ex038.py
@@ -1,2 +1,15 @@
 # EXERCÍCIO 38
-#
+# Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+# O primeiro valor é maior
+# O segundo valor é maior
+# Não existe valor maior, os dois são iguais
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+
+if(num1 > num2):
+    print('O \33[36mPRIMEIRO\33[m valor é maior.')
+elif(num1 < num2):
+    print('O \33[36mSEGUNDO\33[m valor é maior.')
+else:
+    print('Os dois valores são \33[36mIGUAIS\33[m.')
