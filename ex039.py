4cf6193f9e0d4e776b5ff30f7f8765d94eba79c2
lucasllimati <lucasllimati@gmail.com>
2020-08-30 21:15:17 -0300

    exercicios

--- ex039.py
+++ ex039.py
@@ -1,2 +1,18 @@
 # EXERCÍCIO 39
-#
+# Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+from datetime import date
+
+anonasc = int(input('Digite o seu ano de nascimento: '))
+anoatual = date.today().year
+anoalist = anonasc + 18
+idade = anoatual - anonasc
+
+print('Quem nasceu em \33[33m{}\33[m tem \33[33m{}\33[m anos em \33[33m{}\33[m.'.format(anonasc, idade, anoatual))
+
+if idade > 18:
+    print('Você já deveria ter se alistado há \33[36m{}\33[m anos. Seu alistamento foi em \33[36m{}\33[m.'.format(anoatual - anoalist, anoalist))
+elif idade == 18:
+    print('Você deve se alistar \33[31mIMEDIATAMENTE\33[m.')
+else:
+    print('Ainda falta \33[32m{}\33[m anos para o alistamento. Seu alistamento será em \33[32m{}\33[m.'.format(anoalist - anoatual, anoalist))
--- ex040.py
+++ ex040.py
@@ -1,2 +1,18 @@
 # EXERCÍCIO 40
-#
+# Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+# Média 7.0 ou superior: APROVADO
+# Média entre 5.0 e 6.9: RECUPERAÇÃO
+# Média abaixo de 5.0: REPROVADO
+
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print('Tirando {} e {}, a média do aluno é {}.'.format(nota1, nota2, media))
+if media <= 7.0:
+    print('O aluno está \33[36mAPROVADO\033[m')
+elif media >= 5.0 and media >= 6.9:
+    print('O aluno está de \33[33mRECUPERAÇÃO\033[m.')
+else:
+    print('O aluno está \33[31mREPROVADO\033[m.')
