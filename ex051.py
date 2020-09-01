ce04a45cf618a5ded1efe47bbf88cc3dc14eadbd
lucasllimati <lucasllimati@gmail.com>
2020-09-01 18:02:29 -0300

    exercicio

--- ex051.py
+++ ex051.py
@@ -1,2 +1,15 @@
 # EXERCÍCIO 51
-#
+# Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+
+print('=' * 40)
+print('\t10 TERMOS DE UMA PA')
+print('=' * 40)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+
+print('\33[32mINICIO\33[m', end=' → ')
+for num in range(primeiro, decimo + razao, razao):
+    print('{}'.format(num), end=' → ')
+print('\33[31mACABOU\33[m')
--- ex052.py
+++ ex052.py
@@ -1,2 +1,24 @@
 # EXERCÍCIO 52
-#
+# Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+from time import sleep
+
+cont = 0
+num = int(input('Digite um número: '))
+
+print('Calculando...')
+sleep(1)
+
+for i in range(1, num + 1, 1):
+    if num % 1 == 0 and num % i == 0:
+        print('\33[33m', end=' ')
+        cont += 1
+    else:
+        print('\33[35m', end=' ')
+    print('{}'.format(i), end=' ')
+print('\33[m')
+print('\nO número \33[36m{}\33[m foi divisível \33[36m{}\33[m vezes.'.format(num, cont))
+if cont == 2:
+    print('E por isso ele \33[32mÉ PRIMO\33[m.')
+else:
+    print('E por isso ele \33[31mNÃO É PRIMO\33[m.')
