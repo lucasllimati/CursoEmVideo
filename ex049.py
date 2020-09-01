45caa8afafbb00ab61fd4d800d5a09ff7761899f
lucasllimati <lucasllimati@gmail.com>
2020-09-01 16:58:04 -0300

    exercicio

--- ex049.py
+++ ex049.py
@@ -1,2 +1,12 @@
 # EXERCÍCIO 49
-#
+# Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+print('-=-' * 10, end=' ')
+print('TABUADA', end=' ')
+print('-=-' * 10)
+
+num = int(input('Digite um número oara ver sua tabuada: '))
+
+for i in range (0, 11):
+    print('{} x {} = {}'.format(i, num, i * num))
+print('\n\nFIM')
--- ex050.py
+++ ex050.py
@@ -1,2 +1,10 @@
 # EXERCÍCIO 50
-#
+# Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+cont = 0
+soma = 0
+for i in range(1, 7):
+    num = int(input('Digite o {}º número: '.format(i)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Você informou {} número \33[36mPARES\33[m e a soma foi {}.'.format(cont, soma))
