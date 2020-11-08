8bca151b00f20578fdc00642f7364a784d503225
lucasllimati <lucasllimati@gmail.com>
2020-11-07 21:54:49 -0300

    exercicios

--- Desafios/Mundo 3/ex088.py
+++ Desafios/Mundo 3/ex088.py
@@ -1,4 +1,39 @@
-# 79
-# 
+# 88
+# Faça um programa que ajude um jogador da MEGA SENA a criar palpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 
 # Description
+from random import randint
+from time import sleep
+
+lista = []
+jogos = []
+cont = 0
+total = 1
+
+print('-' * 33)
+print('\tJOGO NA MEGA SENA')
+print('-' * 33)
+
+quantjogos = int(input('Quantos jogos você quer que eu sorteie? '))
+
+while total <= quantjogos:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+print('-=' * 5, f' SORTEANDO {quantjogos} JOGOS ', '-=' * 5)
+print()
+for i, l in enumerate(jogos):
+    print(f'Jogo {i + 1}: {l}')
+    sleep(1)
+print()
+print('-=' * 5, f' < BOA SORTE! > ', '-=' * 5)
+
--- README.md
+++ README.md
@@ -134,7 +134,7 @@ NUMERO | DESCRIÇÃO
 85 | Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 86 | Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
 87 | Aprimore o desafio anterior, mostrando no final: A) A soma de todos os valores pares digitados. B) A soma dos valores da terceira coluna. C) O maior valor da segunda linha.
-88 | 
+88 | Faça um programa que ajude um jogador da MEGA SENA a criar palpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 89 | 
 90 | 
 91 | 
